619e9bf643960cae91695e87926b00116ef6a5b0
oyshisarker2001 <01oyshipronomy@gmail.com>
2025-02-19 00:53:56 -0800

    gihub run actions

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,17 +1,22 @@
 import express from 'express';
+import { Router } from "express";
+import userRoutes from "./routes/user-routes.js";
+import chatRoutes from "./routes/chat-routes.js";
 import { config } from 'dotenv';
 import morgan from 'morgan';
-import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 config();
 const app = express();
+const appRouter = Router();
 // middleware
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // remove  it in production
 app.use(morgan("dev"));
-app.use("/api/v1", appRouter);
+appRouter.use("/user", userRoutes); //domain/api/v1/user
+appRouter.use("/chat", chatRoutes); //domain/api/v1/chats
+app.use("/", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
