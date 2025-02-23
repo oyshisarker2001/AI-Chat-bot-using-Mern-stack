0a40581c7b3abd82db237f04dab49a21ea5b39b1
oyshisarker2001 <01oyshipronomy@gmail.com>
2025-02-22 18:38:27 -0800

    github actions

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,13 +9,14 @@ import axios from 'axios';
 import {Toaster } from "react-hot-toast";
 
 
-axios.defaults.baseURL="http://34.234.64.255:5000/api";
+axios.defaults.baseURL="http://34.234.64.255:5000";
 axios.defaults.withCredentials=true;
 const theme = createTheme({
   typography:{fontFamily:"Roboto Slab, serif ",
     allVariants:{color:"white"},
   },
 });
+
 
 createRoot(document.getElementById('root')!).render(
 <StrictMode>
